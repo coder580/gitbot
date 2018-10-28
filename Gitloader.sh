940c54e8fcfe2343014ef891693f3fe8728aaaef
Ethan Schilling <ethan.p.schilling@gmail.com>
2018-10-28 14:36:40 -0500

    second commit

--- Gitloader.sh
+++ Gitloader.sh
@@ -6,9 +6,8 @@ read -p "what is your github username: " user
 read -p "what is the repo name:" repo
 read -p "what do you want to name your first commit:" commit
 git commit -m "$commit"
-clear
 y='y'
-read -p "please confirm your username is" $user "your readme is" $readme "your repo is" $repo "your commit is" $commit "type y to accept type n to cancel" conf
+read -p "please confirm this information is accurate type y to accept type n to cancel" conf"
 if [ "conf" = "y" ]; then
   git remote add origin git@github.com:$user/$repo.git
   git push -u origin master
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ a simple github uploader tool that makes github easier PS: yes i did use it to u
 
 
 
+
+
