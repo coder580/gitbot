5f71e3859a25df360e76b5a5ce2d259d73a66559
coder580 <ethan.p.schilling@gmail.com>
2018-12-16 12:36:37 -0600

    Update Gitloader.sh

--- Gitloader.sh
+++ Gitloader.sh
@@ -1,4 +1,3 @@
-file="~/.gitload/repos.txt"
 commit=""
 conf=""
 repo=""
@@ -9,18 +8,8 @@ vim README.md
 fi
 git init
 git add *
-if [ ! -f ~/.gitload/user.txt ]; then
-read -p "what is your github username: " usern
-mkdir ~/.gitload
-echo " " >> ~/.gitload/repos.txt
-echo $usern > ~/.gitload/user.txt
-fi
-user="$(cat ~/.gitload/user.txt)"
-if [ ! -f .git/name.txt ]; then
+read -p "what is your github username: " user
 read -p "what is the repo name: " repo
-echo $repo > ~/.gitload/repos.txt
-echo $repo >> .git/name.txt
-fi
 read -p "what do you want to name the commit: " commit
 git commit -m "$commit" 
 n='n'
